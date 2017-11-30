d73a1657a44ef37fa710f1a7f5dfb2c780995b8e
Harry Lu <harryzlu14@gmail.com>
2017-11-30 17:29:19 -0500

    changed path resolve to path join

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const path = require('path');
-app.use( express.static(path.resolve(__dirname+ '/mbrace_front/build')));
+app.use( express.static(path.join(__dirname, '/mbrace_front/build')));
 
 const knex = require('knex')({
      client: 'postgres',
@@ -500,7 +500,7 @@ app.delete('/follow/:id', authorize, (req, res)=>{
 });
 
 app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, './mbrace_front/build/index.html'));
+     res.sendFile(path.join(__dirname, './mbrace_front/build/index.html'));
 });
 
 app.listen(PORT, () => {
